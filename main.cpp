0df9d2a5283a199b1e2126c5e01586c64aa9de9f
MaRiamNaser <38191133+MaRiamNaser@users.noreply.github.com>
2018-04-09 02:02:16 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,72 +1,119 @@
 #include <iostream>
-#include <fstream>
-#include <math.h>
-#include <string>
 using namespace std;
-
-
-
-int change(char &ch)
-{
-    if (ch == 'I')
-        return 1;
-    if (ch == 'V')
-        return 5;
-    if (ch == 'X')
-        return 10;
-    if (ch == 'L')
-        return 50;
-    if (ch == 'C')
-        return 100;
-    if (ch == 'D')
-        return 500;
-    if (ch == 'M')
-        return 1000;
-
-
-}
-
 int main()
+{
+    int n;
+    char answer = ' ';
 
-{int sum=0;
+    cout <<" would you like to covert numbers ?(Y/N): ";
+    cin >> answer;
+    answer = toupper(answer);
+    if(answer == 'Y')
+    {
+            again:
+            cout<<endl;
+            cout << "Enter the number: ";
 
-string arr="";
-cout<<"enter the roman number in capital leeters dont forget ya donkey :";
-cin>>arr;
-for(int i=0;i<arr.length();i++){
- int z=change(arr[i]);
+        while(cin>>n)
+        {
+            int i,j,k,l;
+            l=(n/1000)*1000;
+            if(l==1000)
+                cout<<"M";
+            if(l==2000)
+                cout<<"MM";
+            if(l==3000)
+                cout<<"MMM";
+            if(l==4000)
+                cout<<"-IV";
+            if(l==5000)
+                cout<<"-V";
+            if(l==6000)
+                cout<<"-VI";
+            if(l==7000)
+                cout<<"-VII";
+            if(l==8000)
+                cout<<"-VIII";
+            if(l==9000)
+                cout<<"-IX";
+            if(l==10000)
+                cout<<"-X";
+            k=(n%1000);
+            k=(k/100)*100;
+            if (k == 100)
+                cout<<"C";
+            else if (k == 200)
+                cout<<"CC";
+            else if (k == 300)
+                cout<<"CCC";
+            else if (k == 400)
+                cout<<"CD";
+            else if (k ==500)
+                cout<<"D";
+            else if (k == 600)
+                cout<<"DC";
+            else if (k == 700)
+                cout<<"DCC";
+            else if (k ==800)
+                cout<<"DCCC";
+            else if (k == 900)
+                cout<<"CM";
+            k=n%1000;
+            l=k%100;
+            j=(l/10)*10;
+            if (j == 10)
+                cout<<"X";
+            else if (j == 20)
+                cout<<"XX";
+            else if (j == 30)
+                cout<<"XXX";
+            else if (j == 40)
+                cout<<"XL";
+            else if (j ==50)
+                cout<<"L";
+            else if (j == 60)
+                cout<<"LX";
+            else if (j == 70)
+                cout<<"LXX";
+            else if (j ==80)
+                cout<<"LXXX";
+            else if (j == 90)
+                cout<<"XC";
+            i=l%10;
+            if (i == 1)
+                cout<<"I";
+            else if (i == 2)
+                cout<<"II";
+            else if (i == 3)
+                cout<<"III";
+            else if (i == 4)
+                cout<<"IV";
+            else if (i ==5)
+                cout<<"V";
+            else if (i == 6)
+                cout<<"VI";
+            else if (i == 7)
+                cout<<"VII";
+            else if (i ==8)
+                cout<<"VIII";
+            else if (i == 9)
+                cout<<"IX";
+            cout<<endl;
 
-cout<<z<<" ";
+            cout <<"Would you like to try again?(Y/N): ";
+            cin >> answer;
+            answer = toupper(answer);
 
+            if(answer == 'Y' )
+            {
+                goto again;
+            }
+            else if(answer == 'N')
+            {
+                return 0;
+            }
+        }
 
-
-}
-
-for(int i=0;i<arr.length();){
-
- int z=change(arr[i]);
- int y=change(arr[i+1]);
-
-if(y>z){sum=sum+(y-z);i+=2;}
-else{
-    sum+=change(arr[i]);
-    i++;
     }
-cout<<endl<<sum;
-
-
+    return 0;
 }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
